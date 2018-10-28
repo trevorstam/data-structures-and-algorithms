699fb627c006c2ef969954ba8768c6e59d4a1625
trevorstam <trevorstam@gmail.com>
2018-10-28 15:52:43 -0700

    code challenge done, readme added

---
+++ stacks-and-queues/README.md
@@ -0,0 +1,28 @@
+## Code Challenge: Stacks and Queues
+
+#### Author
+Trevor Stam
+
+### Feature Tasks
+
+- Create a Node class that has properties for the value stored in the Node, and a pointer to the next node
+- Create a Stack class that has a top property. It creates an empty Stack when instantiated.
+
+    - This object should be aware of a default empty value assigned to top when the stack is created.
+    - Define a method called push which takes any value as an argument and adds a new node with that value to the top of the stack with an O(1) Time performance.
+    - Define a method called pop that does not take any argument, removes the node from the top of the stack, and returns the node.
+    - Define a method called peek that does not take an argument and returns the node located on the top of the stack.
+
+- Create a Queue class that has a top property. It creates an empty queue when instantiated.
+
+    - This object should be aware of a default empty value assigned to front when the queue is created.
+    - Define a method called enqueue which takes any value as an argument and adds a new node with that value to the back of the queue with an O(1) Time performance.
+    - Define a method called dequeue that does not take any argument, removes the node from the front of the queue, and returns the node.
+    - Define a method called peek that does not take an argument and returns the node located in the front of the stack.
+
+### Node modules:
+jest, eslint
+
+### Sources:
+Code Fellows curriculum, JB Tellez
+
--- stacks-and-queues/stacks-and-queues.js
+++ stacks-and-queues/stacks-and-queues.js
@@ -1,44 +1,80 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
 //create a class of stack
 class Stack {
   constructor() {
-    let storageNode = [];
+    this.top = null;
+    // Array method for reference: let storageNode = [];
   }
-  push(node) {
-    this.storageNode.push(node);
-    //input is a node
+  push(value) {
+    // Array method for reference: this.storageNode.push(value);
     //create a newNode
+    let newNode = new Node(value);
     //point the next of the newNode to the one that is currently Top
-    //newNode.next = top
+    newNode.next = this.top;
     //assign the top to the newNode
-    //top = newNode
+    this.top = newNode;
   }
 
   pop() {
-    this.storageNode.pop();
+    // Array method for reference:this.storageNode.pop();
     //node top is temp
+    let tempNode = this.top;
     //top = top.next
-    //temp.next = null
+    this.top = tempNode.next;
+    //tempnode.next = null
+    tempNode.next = null;
     //return the temp
-
+    return tempNode;
   }
 
   peek() {
-    //return top
-    return this.storageNode[this.storageNode.length - 1];
+    // Array method for reference:return this.storageNode[this.storageNode.length - 1];
+    return this.top;
   }
 }
 
 class Queue {
   constructor() {
-    let qNode = [];
+    this.front = null;
+    this.rear = null;
+    // let qNode = [];
   }
-  enqueue() {
-
+  enqueue(value) {
+    // Array method for reference:this.qNode.unshift(value);
+    let newNode = new Node(value);
+    //if there's just one node queued up
+    if (this.front === null) {
+      this.front = newNode;
+      this.rear = newNode;
+    } else { // assign the pointer of the rear to the newnode and move the rear to the newNode
+      this.rear.next = newNode;
+      this.rear = newNode;
+    }
   }
   dequeue() {
+    // Array method for reference:this.qNode.pop();
+    //create tempNode and point it to the front
+    let tempNode = this.front;
+
+    //re-assign front to the next node in the queue
+    this.front = this.front.next;
+    //to sever all ties from the queue, let the next of the tempNode point to null
+    tempNode.next = null;
 
   }
   peek() {
-
+    // Array method for reference:return this.qNode[0];
+    return this.front;
   }
 }
+
+module.exports = {
+  Stack,
+  Queue,
+};
