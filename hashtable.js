d5026804f3177b76626fc92377a1bf6b0b71dd5e
trevorstam <trevorstam@gmail.com>
2018-11-24 23:54:53 -0800

    stuck in deserialize. Don't know what to do from here

--- hashtable.js
+++ hashtable.js
@@ -6,14 +6,14 @@ class HashMap {
     this.map = new Array(size);
   }
 
-  hash(key) {
+  getHash(key) {
     return key.split('').reduce((acc, cur) => {
       return acc + cur.charCodeAt(0)
     }, 0) % this.size;
   }
 
   add(key, value) {
-    let hash = this.hash(key);
+    let hash = this.getHash(key);
     console.log(hash, key, value);
 
     if (!this.map[hash]) {
@@ -25,24 +25,45 @@ class HashMap {
   }
 
   get(key) {
-    let hash = this.hash(key);
+    let hash = this.getHash(key);
     return this.map[hash];
   }
 
   find(key) {
-    let hash = this.hash(key);
-    return this.map[hash].find((val, idx, obj) => {
-      //still figuring this out
-    })
+    let hash = this.getHash(key);
+
+    if (this.map[hash]) {
+      let foundKey = this.map[hash].find((items) => {
+        return Object.keys(items) === key;
+      });
+    }
+
+    if (!foundKey) {
+      return 'No existing key discovered';
+    } else {
+      return foundKey[key];
+    }
   }
 
   contains(key) {
-    let hash = this.hash(key);
+    let hash = this.getHash(key);
     if (!this.map[hash]) {
-      return 'No key';
+      return false;
     } else {
       return `It has key: ${key}`;
     }
+  }
+
+  serialize() {
+    return JSON.stringify(this.map);
+  }
+
+  deserialize(data) {
+    let deserializedString = JSON.parse(data);
+    let newHashMap = new HashMap();
+    //stuck in this method. Don't know how to move on
+
+
   }
 
 }
