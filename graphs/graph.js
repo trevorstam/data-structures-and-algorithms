b3bc8b7dd8a785d1791f989a6b17634b2a287821
trevorstam <trevorstam@gmail.com>
2018-12-03 14:41:24 -0800

    breadthfirst traversal added

--- graphs/graph.js
+++ graphs/graph.js
@@ -46,6 +46,31 @@ class Graph {
   size(){
     return this._adjacencyList.size;
   }
+
+  breadthFirst(startVertex) {
+    const trackingQueue = [];
+    const seenVertex = new Set();
+
+    trackingQueue.push(startVertex);
+    seenVertex.add(startVertex);
+
+    while (trackingQueue.length) {
+      const currentVertex = trackingQueue.shift();
+
+      const connected = this.getNeighbors(currentVertex);
+
+      for (let neighbor of connected) {
+        const neighborVertex = neighbor.vertex;
+        if (seenVertex.has(neighborVertex)) {
+          continue;
+        } else {
+          seenVertex.add(neighborVertex);
+        }
+        trackingQueue.push(neighborVertex);
+      }
+    }
+    return seenVertex;
+  }
 }
 
 const graph = new Graph();
